485fe741ddd7372442d008e2a2692dd96b12f22a
JohnNasitem <jnasitem1@nait.ca>
2025-09-17 22:06:50 -0600

    added logger

---
+++ Services/Logger/ILogger.cs
@@ -0,0 +1,25 @@
+﻿//***********************************************************************************
+//Program: ILogger.cs
+//Description: Logger interface
+//Date: Sept 17, 2025
+//Author: John Nasitem
+//***********************************************************************************
+
+
+
+using static PayorLedger.Services.Logger.Logger;
+
+
+
+namespace PayorLedger.Services.Logger
+{
+    public interface ILogger
+    {
+        /// <summary>
+        /// Add log to the current file
+        /// </summary>
+        /// <param name="message">Message to add</param>
+        /// <param name="type">Type of log</param>
+        public void AddLog(string message, LogType type);
+    }
+}
---
+++ Services/Logger/Logger.cs
@@ -0,0 +1,51 @@
+﻿//***********************************************************************************
+//Program: Logger.cs
+//Description: Logs actions done by user and any exceptions
+//Date: Sept 17, 2025
+//Author: John Nasitem
+//***********************************************************************************
+
+
+
+using System.IO;
+
+
+
+namespace PayorLedger.Services.Logger
+{
+    public class Logger : ILogger
+    {
+        private static readonly string _filePath = $"Logs/{DateTime.UtcNow.ToString("yyyy-MM-dd")}";
+
+
+
+        public Logger()
+        {
+            // Create log file
+            AddLog("User openned application.", LogType.Action);
+        }
+
+
+
+        /// <summary>
+        /// Add log to the current file
+        /// </summary>
+        /// <param name="message">Message to add</param>
+        /// <param name="type">Type of log</param>
+        public void AddLog(string message, LogType type)
+        {
+            File.WriteAllText(_filePath, $"[{DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss")}] {type.ToString().ToUpper()}: {message + Environment.NewLine}");
+        }
+
+
+
+        /// <summary>
+        /// Log type
+        /// </summary>
+        public enum LogType
+        {
+            Action = 0,
+            Error = 1,
+        }
+    }
+}
