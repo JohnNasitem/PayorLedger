1a43341da3e07dfa83f8248e0e99a2cc76d5e1a6
JohnNasitem <jnasitem1@nait.ca>
2025-09-25 21:07:14 -0600

    added class to hold tab content for payor report

---
+++ Models/TotalTab.cs
@@ -0,0 +1,37 @@
+﻿//***********************************************************************************
+//Program: TotalTab.cs
+//Description: Tab content for total type
+//Date: Sep 25, 2025
+//Author: John Nasitem
+//***********************************************************************************
+
+
+
+using System.Data;
+
+
+
+namespace PayorLedger.Models
+{
+    public class TotalTab
+    {
+        /// <summary>
+        /// Name of the total type
+        /// </summary>
+        public string Name { get ; set; }
+
+
+
+        /// <summary>
+        /// Datatable of the tab
+        /// </summary>
+        public DataTable Content { get; set; } = new();
+
+
+
+        public TotalTab(RowEntry.RowLabel type)
+        {
+            Name = Enum.GetName(type)!;
+        }
+    }
+}
